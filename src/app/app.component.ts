1940036425905fac94aa74ac8b7a638c9ad40d8c
Domo <stolarczyk.dominik@gmail.com>
2019-01-24 20:01:48 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { state, style, trigger, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('imgState', [state('normal', style({ transform: 'translateX(100px)' })),
+    state('moved', style({ transform: 'translateX(0)' })),
+    transition('normal => moved', animate(300)),
+    transition('moved => normal', animate(1000))
+    ])]
 })
 export class AppComponent {
+  state = 'normal';
   title = 'animations';
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'moved' : this.state = 'normal';
+
+  }
 }
